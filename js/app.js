9f144e1fda895ac1fdce751d40ceba24de2b1bc0
adsweb4you <adsweb4you@gmail.com>
2022-12-14 20:56:13 +0400

    55

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
-let elem = document.getElementById('app');
+// var y = 6;
+// let x = 5;
+// const z = 8;
 
-elem.addEventListener('click', function(){
-    this.innerText = 'ახალი მნიშვნელობა'
+ 
+// console.log(x * y);
+
+let input  = document.getElementById('valid');
+let output = document.getElementById('output');
+input.addEventListener('input',function(){
+    let value  = this.value;
+    if (value.length < 8) {
+         output.innerHTML = 'ტექსტში სიმბოლოების რაოდენობა უნდა იყოს 8 სიმბოლოზე მეტი';
+         this.classList.add('border-danger');
+         this.classList.remove('border-success');
+         tst.disabled = true;
+    }else{
+        output.innerHTML = ''
+        this.classList.remove('border-danger');
+        this.classList.add('border-success');
+        tst.disabled = false;
+    }
+
+    let regs = value.search(/[@ _ & ) ( .* A-Z]$/g);
+ 
+   if (regs == -1) {
+    output.innerHTML += '<br>' + 'ტექსტში უნდა ერიოს სიმბოლოები @ _ & ) (';
+    this.classList.add('border-danger');
+    this.classList.remove('border-success');
+    tst.disabled = true;
+   }
 })
